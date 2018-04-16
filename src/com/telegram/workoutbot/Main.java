583628ec993ca4fa8bd907b82782907a0b737bde
Slavvok <slavvok@gmail.com>
2018-04-16 22:46:17 +0300

    Commit

---
+++ src/com/telegram/workoutbot/Main.java
@@ -0,0 +1,19 @@
+package com.telegram.workoutbot;
+
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.TelegramBotsApi;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
+
+public class Main {
+
+	public static void main(String[] args) {
+		ApiContextInitializer.init();
+		TelegramBotsApi botsApi = new TelegramBotsApi();
+		try {
+			botsApi.registerBot(new WorkoutBot());
+		} catch (TelegramApiException e) {
+			
+		}
+	}
+
+}
